0823c6cde6aea9a8be62db607c74b37802943c92
Zain Memon <zain@inzain.net>
2019-08-08 18:31:18 -0700

    Redo models to be less verbose

--- server.py
+++ server.py
@@ -17,82 +17,62 @@ db.app = app
 db.init_app(app)
 
 
+class Team(db.Model):
+    """Team in the r00tz27 game."""
+
+    __tablename__ = "teams"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    name_plural = db.Column(db.String(255), nullable=False)
+
+    players = db.relationship("Player", backref="team")
+
+    def __repr__(self):
+        """Show team name."""
+        return f"<Team name={self.name}>"
+
+
 class Player(db.Model):
     """R00tz27 game player."""
 
     __tablename__ = "players"
 
-    player_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    team_id = db.Column(db.Integer, db.ForeignKey("teams.team_id"))
-    games_played = db.Column(db.Integer, default=0, nullable=False)
-    games_won = db.Column(db.Integer, default=0, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    mac = db.Column(db.String(255), nullable=False, unique=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
 
-    team = db.relationship("Team", backref="players", uselist=False)
-    games = db.relationship("Game", secondary="game_players", backref="players")
+    # games = # TODO
 
     def __repr__(self):
         """Show info about player."""
-
-        return f"<Player player_id={ self.player_id } " f"username={ self.username }>"
+        return f"<Player id={ self.id } name={ self.name }>"
 
 
 class Game(db.Model):
     """R00tz27 game."""
 
     __tablename__ = "games"
+    __table_args__ = (
+        db.UniqueConstraint(
+            "challenge", "player1_id", "player2_id", name="unique_players_challenge"
+        ),
+    )
 
-    game_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     challenge = db.Column(db.Integer, nullable=False)
-    player1_id = db.Column(db.Integer, db.ForeignKey("players.player_id"))
-    player1_win = db.Column(db.Boolean)
-    player2_id = db.Column(db.Integer, db.ForeignKey("players.player_id"))
-    player2_win = db.Column(db.Boolean)
-    initiated_at = db.Column(db.DateTime)
 
-    player1 = db.relationship(
-        "Player", primaryjoin="Game.player1_id == Player.player_id"
-    )
-    player2 = db.relationship(
-        "Player", primaryjoin="Game.player2_id == Player.player_id"
-    )
+    player1_id = db.Column(db.Integer, db.ForeignKey("players.id"))
+    player1_win = db.Column(db.Boolean)
+    player2_id = db.Column(db.Integer, db.ForeignKey("players.id"))
+    player2_win = db.Column(db.Boolean)
+
+    played_at = db.Column(db.DateTime)
 
     def __repr__(self):
         """Show game details."""
-
-        return (
-            f"<Game game_id={ self.game_id } player1={ self.player1_id } "
-            f"player2={ self.player2_id }>"
-        )
-
-
-class Team(db.Model):
-    """Team in the r00tz27 game."""
-
-    __tablename__ = "teams"
-
-    team_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    games_played = db.Column(db.Integer, default=0, nullable=False)
-    games_won = db.Column(db.Integer, default=0, nullable=False)
-
-    def __repr__(self):
-        """Show team name."""
-
-        return f"""<Team name={self.name}>"""
-
-
-class GamePlayer(db.Model):
-    """Association between games and players."""
-
-    __tablename__ = "game_players"
-
-    gameplayer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey("games.game_id"))
-    player_id = db.Column(db.Integer, db.ForeignKey("players.player_id"))
-
-    game = db.relationship("Game", uselist=False, backref="game_player_objects")
-    player = db.relationship("Player", uselist=False, backref="game_player_objects")
+        return f"<Game id={ self.id } player1={ self.player1_id } player2={ self.player2_id }>"
 
 
 def seed_teams():
@@ -100,11 +80,11 @@ def seed_teams():
     team_names = [team.name for team in Team.query.all()]
 
     if "tiger" not in team_names:
-        team1 = Team(team_id=1, name="tiger")
+        team1 = Team(id=0, name="tiger", name_plural="tigers")
         db.session.add(team1)
 
     if "wolf" not in team_names:
-        team2 = Team(team_id=2, name="wolf")
+        team2 = Team(id=1, name="wolf", name_plural="wolves")
         db.session.add(team2)
 
     db.session.commit()
